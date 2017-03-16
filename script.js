a5a9d9edf3c13b7eca6a03c5e3aeaee46f90f3b5
Jonathan Austin <jonathan.austin@arm.com>
2017-03-16 13:20:38 +0000

    Update for actual usage

--- script.js
+++ script.js
@@ -7,22 +7,26 @@ $.ajax({
     url: url ,
     dataType: 'jsonp',
     success: function (data) { 
-       console.log(data.meta);
-       $.each(data.data, function(index, element) {
+       console.log(data);
+       $.each(data.results, function(index, element) {
             if (element.status == "upcoming") { console.log(data.data[index - 1]); };
-            if (element.name.lastIndexOf("See the space", 0) !== 0
+            //$('#pages').append(template(element));
+           /* if (element.name.lastIndexOf("See the space", 0) !== 0
                 & (element.name.lastIndexOf("Thursday Pub", 0) !== 0)
                 & (element.name.lastIndexOf("See the Space", 0) !== 0)
                 & (element.name.lastIndexOf("Family Makers", 0) !== 0)
                 & (element.name.lastIndexOf("K1 Cohousing", 0) !== 0)
                 & (element.name.lastIndexOf("OpenDCU.org", 0) !== 0)
                 ) {
-            	$('body').append(template(element));
+            	$('#pages').append(template(element));
+	}*/
+           if (element.yes_rsvp_count > 1){
+        	$('#pages').append(template(element));
 	}
         });
-       if (data.data[0].status === "past") {
-            	$('body').append("<h1>HERE</h1>");
-       	    get_meetup(data.meta.next_link);
+       if (data.results[0].status === "past") {
+            	//$('body').append("<h1>HERE</h1>");
+       	    get_meetup(data.meta.next);
 	}
     }
 });
@@ -36,11 +40,11 @@ template = Handlebars.compile(source);
 var context = {name: "My New Post", yes_rsvp_count: "20", description: "This is my first post!"};
 var html    = template(context);
 
-$( ".main" ).append( html );
+//$( ".main" ).append( html );
 
 //var first_url = 'https://api.meetup.com/makespace/events?&sign=true&status=past' 
-var first_url = 'https://api.meetup.com/makespace/events?&sign=true&status=past' 
-//var first_url = 'https://api.meetup.com/2/events?&sign=true&photo-host=public&group_urlname=makespace&status=past&page=5000'
+//var first_url = 'https://api.meetup.com/makespace/events?&sign=true&status=past' 
+var first_url = 'https://api.meetup.com/2/events?&sign=true&photo-host=public&group_urlname=makespace&status=past&page=5000'
 
 get_meetup(first_url);
 
